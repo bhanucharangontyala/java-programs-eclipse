24e8560ee96e02a22c5863e175b75bed558fb2a3
bhanucharangontyala <bhanucharangontyala123@gmail.com>
2026-02-19 13:31:04 +0530

    19/02/2026

--- Bhanu/src/com/ibmpractice/Anagram.java
+++ Bhanu/src/com/ibmpractice/Anagram.java
@@ -1,29 +1,45 @@
 package com.ibmpractice;
 
+import java.util.Arrays;
+
 public class Anagram {
 
 	public static void main(String[] args) {
-		int[] arr = new int[26];
+//		int[] arr = new int[26];
 		String str1 = "silent";
-		String str2 = "listan";
-		boolean flag = true;
-		if (str1.length() != str2.length()) {
-			System.out.println("Not Anagram");
-		} else {
-			for (int i = 0; i < str1.length(); i++) {
-				arr[str1.charAt(i) - 'a'] += 1;
-				arr[str2.charAt(i) - 'a'] -= 1;
-			}
-		}
-		for (int ar : arr) {
-			if (ar != 0) {
-				flag = false;
-			}
-		}
-		if (flag) {
+		String str2 = "listen";
+//		boolean flag = true;
+//		if (str1.length() != str2.length()) {
+//			System.out.println("Not Anagram");
+//		} else {
+//			for (int i = 0; i < str1.length(); i++) {
+//				arr[str1.charAt(i) - 'a'] += 1;
+//				arr[str2.charAt(i) - 'a'] -= 1;
+//			}
+//		}
+//		for (int ar : arr) {
+//			if (ar != 0) {
+//				flag = false;
+//			}
+//		}
+//		if (flag) {
+//			System.out.println("Anagram");
+//		} else {
+//			System.out.println("Not Anagram");
+//		}
+		
+		
+		//or
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		if(Arrays.equals(arr1, arr2)) {
 			System.out.println("Anagram");
-		} else {
-			System.out.println("Not Anagram");
+		}else {
+			System.out.println("Not a Anagram");
 		}
 	}
 
